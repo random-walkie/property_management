0d159da7be1ee8c94ed23b294ac1d2a72c1e41a9
random-walkie <26143642+random-walkie@users.noreply.github.com>
2024-12-10 18:53:59 +0000

    understanding PathVariable annotation

--- src/main/java/com/mycompany/property_management/controller/CalculatorController.java
+++ src/main/java/com/mycompany/property_management/controller/CalculatorController.java
@@ -1,9 +1,6 @@
 package com.mycompany.property_management.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/calculator")
@@ -14,5 +11,17 @@ public class CalculatorController {
     // http://localhost:8080/api/v1/calculator/add?num111=6.7&num222=1.3 -> due to @RequestParam
     public Double add(@RequestParam("num1") Double num1, @RequestParam("num2") Double num2) {
         return num1 + num2;
+    }
+
+    // In this example, we will map the values of url to Java variables by Path Variable method.
+    @GetMapping("/subtract/{num1}/{num2}")
+    public Double subtract(@PathVariable("num1") Double num1, @PathVariable("num2") Double num2) {
+        Double result = null;
+        if (num1 > num2) {
+            result = num1 - num2;
+        } else {
+            result = num2 - num1;
+        }
+        return result;
     }
 }
